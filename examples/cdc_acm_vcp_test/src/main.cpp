ea6e7413345ff6bb60022defaa73974277285332
ilyyangminglong <ilyyangminglong@126.com>
2022-05-13 15:23:08 +0800

    更新

---
+++ examples/cdc_acm_vcp_test/src/main.cpp
@@ -0,0 +1,16 @@
+//************************************************************
+// this is a simple example that uses the painlessMesh library
+// 
+// This example shows how to build a mesh with named nodes
+//
+//************************************************************
+#include <arduino>
+
+
+void setup() {
+ 
+}
+
+void loop() {
+
+}
